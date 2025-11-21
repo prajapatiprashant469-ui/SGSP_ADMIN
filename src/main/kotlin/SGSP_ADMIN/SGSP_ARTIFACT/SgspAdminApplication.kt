f887a9f3c6494fabd0ecda67c681c9bed2ccf86d
Prashant Kumar Prajapati <prashant.prajapati@rapipay.com>
2025-11-21 14:22:50 +0530

    first

---
+++ src/main/kotlin/SGSP_ADMIN/SGSP_ARTIFACT/SgspAdminApplication.kt
@@ -0,0 +1,11 @@
+package SGSP_ADMIN.SGSP_ARTIFACT
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SgspAdminApplication
+
+fun main(args: Array<String>) {
+	runApplication<SgspAdminApplication>(*args)
+}
---
+++ src/test/kotlin/SGSP_ADMIN/SGSP_ARTIFACT/SgspAdminApplicationTests.kt
@@ -0,0 +1,13 @@
+package SGSP_ADMIN.SGSP_ARTIFACT
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SgspAdminApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
